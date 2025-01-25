257e39b6163923548f642c6c0d326de127451274
Nizomiddin <nizomiddinmirzanazarov@gmail.com>
2025-01-25 23:46:59 +0500

    changing

--- src/main/java/zeroone/developers/test/repository/UserRepository.java
+++ src/main/java/zeroone/developers/test/repository/UserRepository.java
@@ -5,5 +5,5 @@ import zeroone.developers.test.entity.User;
 import java.util.Optional;
 
 public interface UserRepository extends BaseRepository<User,String>{
-    Optional<User> findByPhoneNumber(String phoneNumber);
+    Optional<User> findByPhoneNumberAndVisibleTrue(String phoneNumber);
 }
